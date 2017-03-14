4c8058b8ca667bacff66643abb8d420d8eb0b9f2
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-03-14 17:32:40 +0530

    6 more Pattern Questions in C++

---
+++ pattern9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int n;
+	cin >> n;
+
+	char spaces = ' ';
+	for( int i = 1; i <= 5; i++ ){
+		for( int j = n - i; j > 0; j-- ){
+			cout << spaces;
+		}
+		for( int j = 1; j <= i; j++ ){
+			cout << i;
+		}
+		cout << endl;
+	}
+
+    return 0;
+
+}
